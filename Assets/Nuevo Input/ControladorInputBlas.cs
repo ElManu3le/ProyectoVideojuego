cc18bd5b3c6a636ed7a5835e43cacc56da85163f
nico-perez <0308nperez@e-itaca.es>
2020-10-07 23:26:24 +0200

    pffffff
    ahora tenemos inputactionasset
    he copiado un poco el controlador rigidbody de standard assets
    y ya

--- Assets/Nuevo Input/ControladorInputBlas.cs
+++ Assets/Nuevo Input/ControladorInputBlas.cs
@@ -6,8 +6,10 @@ using UnityEngine.InputSystem;
 [DisallowMultipleComponent]
 public class ControladorInputBlas : MonoBehaviour
 {
-    public InputActionMap mapa_acciones;
+    public InputActionAsset mapa_acciones;
     public Camera camara;
+
+    private CharacterController contr;
 
     [Range(1f, 30f)] // Sensibilidad de la camara
     public float sens = 15f;
@@ -17,15 +19,14 @@ public class ControladorInputBlas : MonoBehaviour
     private void Update()
     {
         var vec_mov = mapa_acciones["moverse"].ReadValue<Vector2>() * Time.deltaTime * mulv;
-        transform.Translate(vec_mov.x, 0, vec_mov.y);
+        contr.Move(new Vector3(vec_mov.x, -9.8f * Time.deltaTime, vec_mov.y));
 
-        var vec_mir = mapa_acciones["mirar"].ReadValue<Vector2>() * Time.deltaTime * sens;
-        transform.Rotate(new Vector3(0, vec_mir.x, 0), Space.Self);
-        camara.transform.Rotate(new Vector3(-vec_mir.y, 0, 0));
     }
 
     void Awake()
     {
+        contr = GetComponent<CharacterController>();
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         // pa este pues no
