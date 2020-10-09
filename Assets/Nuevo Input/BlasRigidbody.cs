cee45b6887d457be86e2ab152ac1fbe1cacb1b9f
nico-perez <0308nperez@e-itaca.es>
2020-10-09 13:33:35 +0200

    un pokitin mas
    Ahora Blas pesa 69 kg
    Ahora Blas puede saltar
    Ahora Blas puede mirar alrededor con el ratón
    He hecho una placa de presión ahí prototipo en un momentín

--- Assets/Nuevo Input/BlasRigidbody.cs
+++ Assets/Nuevo Input/BlasRigidbody.cs
@@ -15,7 +15,7 @@ public class BlasRigidbody : MonoBehaviour
         public float BackwardSpeed = 4.0f;  // Speed when walking backwards
         public float StrafeSpeed = 4.0f;    // Speed when walking sideways
         public float RunMultiplier = 2.0f;   // Speed when sprinting
-        public KeyCode RunKey = KeyCode.LeftShift;
+
         public float JumpForce = 30f;
         public AnimationCurve SlopeCurveModifier = new AnimationCurve(new Keyframe(-90.0f, 1.0f), new Keyframe(0.0f, 1.0f), new Keyframe(90.0f, 0.0f));
         [HideInInspector] public float CurrentTargetSpeed = 8f;
@@ -125,10 +125,10 @@ public class BlasRigidbody : MonoBehaviour
     {
         RotateView();
 
-        //if (CrossPlatformInputManager.GetButtonDown("Jump") && !m_Jump)
-        //{
-        //    m_Jump = true;
-        //}
+        if (controles["saltar"].ReadValue<float>() > float.Epsilon && !m_Jump)
+        {
+            m_Jump = true;
+        }
     }
 
 
