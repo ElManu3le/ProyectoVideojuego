fc25e686c9661f60706f0747584e1310c9fa7343
nico-perez <0308nperez@e-itaca.es>
2020-10-25 18:14:37 +0100

    pero bro
    Co-Authored-By: ElManu3le <62712661+ElManu3le@users.noreply.github.com>
    Co-Authored-By: alvaro <71507502+alvarosilvabolea@users.noreply.github.com>

---
+++ Assets/Scripts/ZonaMuerte.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ZonaMuerte : MonoBlashaviour
+{
+    public static int vidas_restantes = 3;
+
+    private AudioSource audio;
+    public Transform spawn;
+
+    private void Start()
+    {
+        audio = Blas.GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        Debug.Log("Muerte! A!");
+        if (other.gameObject.layer == 10)
+        {
+            
+            if (--vidas_restantes <= 0)
+            {
+                vidas_restantes = 3;
+                SceneManager.LoadScene("menu_inicio");   
+            }
+            else
+            {
+                audio.Play();
+                StartCoroutine(moricion());
+            }
+        }
+    }
+
+    private IEnumerator moricion()
+    {
+        yield return new WaitForSeconds(.38f);
+        Blas.transform.position = spawn.position;
+    }
+}
