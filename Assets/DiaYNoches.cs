e4765e054095ccb489a3d4b6d6d599bfcad7bf25
Nicolás Pérez <71397273+nico-perez@users.noreply.github.com>
2020-11-11 18:35:41 +0100

    diaynoche

---
+++ Assets/DiaYNoches.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiaYNoches : MonoBehaviour
+{
+
+    public float v = 10.5f;
+    public Color a,b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, transform.right, v * Time.deltaTime);
+        RenderSettings.ambientIntensity = Vector3.Angle(transform.forward, Vector3.up) / 180f;
+    }
+}
